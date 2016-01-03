08fed65408ffcad9a885758ef0136773c9a1e0b8
nemo <nemo@nemor.cz>
2016-01-03 17:59:29 +0100

    Water layer script

---
+++ scripts/getWater.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import socket
+
+HOST = '10.0.0.173'    # The remote host
+PORT = 9011              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('GZ\r\n')
+data = s.recv(10)
+s.close()
+print 'Received'
+print data
